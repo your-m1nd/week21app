f475598e293aa0c5394ec19f30e30af40606824e
your-m1nd <vi.neurogirl@gmail.com>
2023-08-30 21:32:11 +0300

    add react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
 
-function App() {
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Учёба", 4],
+  ["Еда", 1],
+  ["Прогулка", 3],
+  ["Время с ребенком", 15],
+  ["Сон", 4],
+  ["Просмотр фильмов/сериалов", 2],
+  ["Время для себя", 1]
+];
+
+export const options = {
+  title: "Моя активность в течение дня",
+};
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"600px"}
+    />
   );
 }
 
-export default App;
